83d75b96af7b24bfea4d3caa068e6995b8f588df
Piotr <ppp16@vp.pl>
2020-10-15 17:20:30 +0200

    Initialization project

---
+++ src/main/java/com/project/CaseKey/CaseKeyApplication.java
@@ -0,0 +1,13 @@
+package com.project.CaseKey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CaseKey/CaseKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CaseKey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
